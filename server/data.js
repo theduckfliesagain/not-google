25cdc0966bc5549a47a43cad350fcf721e25f438
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-03-09 15:19:11 +0000

    added initial sample data for three sites

---
+++ server/data.js
@@ -0,0 +1,6 @@
+const data = [
+    {id: 1, heading: "BBC", url: "https://www.bbc.co.uk/", desc: "The best of the BBC, with the latest news and sport headlines, weather, TV & radio highlights and much more from across the whole of BBC Online."},
+    {id: 2, heading: "YouTube", url: "https://www.youtube.com/", desc: "Enjoy the videos and music you love, upload original content, and share it all with friends, family, and the world on YouTube."},
+    {id: 3, heading: "Google", url: "https://www.google.co.uk/", desc: "Search the world's information, including webpages, images, videos and more."},
+
+]
