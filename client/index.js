6404f881cbbbdb5acd4bdfa967b1e1f4a99ce75f
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-03-10 12:31:53 +0000

    add nav search bar functionality

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
-const form = document.querySelector('form');
+const form = document.querySelectorAll('form');
 const randomBtn = document.querySelector('[name="random"]');
 const searchPage = document.getElementById("search-page")
 const resultsPage = document.getElementById("results-page")
@@ -7,26 +7,25 @@ const nav = document.querySelector('nav');
 
 const url = "http://localhost:3000/"
 
-console.log(randomBtn);
-
 resultsPage.style.display = "none";
 searchPage.style.display = "block";
 
-form.addEventListener('submit', presentResults)
+form.forEach(form => {addEventListener('submit', presentResults)});
 randomBtn.addEventListener('click', presentRandomResult)
 
 function presentResults(e) {
     e.preventDefault();
-    const searchText = document.getElementById('search-bar').value;
-    const routeURL =  searchText ? `search?q=${searchText}` : `results`;
-    let fetchURL =  url + routeURL;
 
+    const searchText = e.target.querySelector('[type="text"]').value;
+    const routeURL =  searchText ? `search?q=${searchText}` : `results`;
+    
+    const fetchURL =  url + routeURL;
+    
     fetch(fetchURL)
         .then(resp => resp.json())
         .then(results => showResults(results))
 
     togglePage();
-
 }
 
 
@@ -37,29 +36,28 @@ function presentRandomResult(e) {
 
     fetch(fetchURL)
         .then(resp => resp.json())
+        .then(result => window.open(result.url))
 }
 
 function togglePage(){
-    // If on the search page switch to the results page and vice versa
-    if(searchPage.style.display === "none"){
-        resultsPage.style.display = "none";
-        searchPage.style.display = "block";
-
-
-    } else {
+    if(resultsPage.style.display === "none"){
         resultsPage.style.display = "block";
         searchPage.style.display = "none";
     }
-
 }
 
 function showResults(results){
+    while(resultSection.firstChild){
+        resultSection.removeChild(resultSection.firstChild);
+    }
+
     if(results.length === 0){
         const noResultElement = document.createElement('p');
         noResultElement.textContent = 'No results, sorry!';
         resultSection.appendChild(noResultElement);
     }
     else {
+        // Only show first 10 results 
         results = results.slice(0, 10);
         results.forEach(result => createResultElement(result));
     }
@@ -67,32 +65,27 @@ function showResults(results){
 
 
 function createResultElement(result){
-    const resultSection = document.getElementById('results');
 
+    const resultSection = document.getElementById('results');
+    // Create new elements
     const resultElement = document.createElement("div");
     const headingElement = document.createElement("h2")
     const urlElement = document.createElement("h4")
     const descElement = document.createElement("p")
     const linkElement = document.createElement("a");
-
+    // Add retrieved result content to elements
     headingElement.textContent = result.heading;
     urlElement.textContent= result.url;
     descElement.textContent = result.desc;
-
+    // Add URL to heading
     linkElement.setAttribute("href", result.url);
-    
+    // Construct result HTML
     linkElement.appendChild(headingElement)
-
-
     resultElement.appendChild(urlElement);
     resultElement.appendChild(linkElement);
     resultElement.appendChild(descElement);
 
     resultElement.className = "search-result"
-
+    
     resultSection.appendChild(resultElement);
-
-
-    console.log(result);
-
 }
