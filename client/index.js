fc9c0100760f2f356ea607b60c29826a41366fc7
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-03-10 09:57:54 +0000

    added functionality to feeling lucky (random) button

--- client/index.js
+++ client/index.js
@@ -1,37 +1,76 @@
 const form = document.querySelector('form');
+const randomBtn = document.querySelector('[name="random"]');
 const searchPage = document.getElementById("search-page")
 const resultsPage = document.getElementById("results-page")
+const resultSection = document.getElementById('results');
 const nav = document.querySelector('nav');
+
+const url = "http://localhost:3000/"
+
+console.log(randomBtn);
 
 resultsPage.style.display = "none";
 searchPage.style.display = "block";
 
 form.addEventListener('submit', presentResults)
+randomBtn.addEventListener('click', presentRandomResult)
 
 function presentResults(e) {
     e.preventDefault();
     const searchText = document.getElementById('search-bar').value;
 
-    if(!searchText) {
-        fetch("http://localhost:3000/results")
-            .then(resp => resp.json())
-            .then(results => results.forEach(result => createResultElement(result)))
-    }
+    let fetchURL =  url + searchText ? `search?q=${searchText}` : `results`;
+
+    fetch(fetchURL)
+        .then(resp => resp.json())
+        .then(results => showResults(results))
+        // .catch(error => console.log(error))
 
     togglePage();
 
     // fetch("localhost:3000/search")
 }
 
+
+function presentRandomResult(e) {
+    e.preventDefault();
+
+    let fetchURL = url + `results/random`;
+
+    fetch(fetchURL)
+        .then(resp => resp.json())
+        .then(result => window.open(result.url))
+        // .catch(error => console.log(error))
+
+    // togglePage();
+
+    // fetch("localhost:3000/search")
+}
+
 function togglePage(){
+    // If on the search page switch to the results page and vice versa
     if(searchPage.style.display === "none"){
         resultsPage.style.display = "none";
         searchPage.style.display = "block";
+
+
     } else {
         resultsPage.style.display = "block";
         searchPage.style.display = "none";
     }
 
+}
+
+function showResults(results){
+    if(results.length === 0){
+        const noResultElement = document.createElement('p');
+        noResultElement.textContent = 'No results, sorry!';
+        resultSection.appendChild(noResultElement);
+    }
+    else {
+        results = results.slice(0, 10);
+        results.forEach(result => createResultElement(result));
+    }
 }
 
 
@@ -47,8 +86,8 @@ function createResultElement(result){
     urlElement.textContent= result.url;
     descElement.textContent = result.desc;
 
-    resultElement.appendChild(headingElement);
     resultElement.appendChild(urlElement);
+    resultElement.appendChild(headingElement);
     resultElement.appendChild(descElement);
 
     resultElement.className = "search-result"
