379aa27099123f214c9ded5298e92f3e03326b38
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-03-10 17:25:27 +0000

    switch to google search engine

--- client/index.js
+++ client/index.js
@@ -20,11 +20,31 @@ function presentResults(e) {
     const routeURL =  searchText ? `search?q=${searchText}` : `results`;
     
     const fetchURL =  url + routeURL;
-  
-    fetch(fetchURL)
-        .then(resp => resp.json())
-        .then(results => showResults(results))
 
+    const googleURL = `https://customsearch.googleapis.com/customsearch/v1`
+    const engineID = `cx=837a8ca977f033aaf`
+    const apiKey = `key=AIzaSyDSt4JzVylIBIfOKHRtSy8E1HnMj3vE_cE`
+    
+    const fetchGoogle = `${googleURL}?${apiKey}&${engineID}&q=${searchText}`;
+
+    fetch(fetchGoogle)
+    .then(resp => resp.json())
+    .then(resp => resp.items)
+    .then(items => {
+        const results = [];
+        items.forEach(item => {
+            const result = {
+            "heading": item.title,
+            "url": item.link,
+            "desc": item.snippet
+            }
+            results.push(result);
+        })
+
+        showResults(results);
+    })
+    .catch(noResultsError)
+  
     togglePage();
 }
 
@@ -52,15 +72,19 @@ function showResults(results){
     }
 
     if(results.length === 0){
-        const noResultElement = document.createElement('p');
-        noResultElement.textContent = 'No results, sorry!';
-        resultSection.appendChild(noResultElement);
+        noResultsError();
     }
     else {
         // Only show first 10 results 
         results = results.slice(0, 10);
         results.forEach(result => createResultElement(result));
     }
+}
+
+function noResultsError () {
+    const noResultElement = document.createElement('p');
+    noResultElement.textContent = 'No results, sorry!';
+    resultSection.appendChild(noResultElement);
 }
 
 
